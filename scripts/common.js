159cdc0fbd567cf6921a34f70c9cd211fcbbf710
Mironenkov Ruslan <mironenkovr@gmail.com>
2016-09-16 15:22:48 +0300

    push index.html

---
+++ scripts/common.js
@@ -0,0 +1,13 @@
+/**
+ * Created by GloryManU on 04.07.2016.
+ */
+(function(){
+    $('.me-reviews-slider').lightSlider({
+        item: 1,
+        controls: false,
+        pager: true,
+        slideMargin: 0,
+        speed: 1000,
+        galleryMargin: 40
+    });
+})();
