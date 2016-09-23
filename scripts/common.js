ff75dc1ae7532a299470e4e1469b9c6cbbbe9e37
Mironenkov Ruslan <mironenkovr@gmail.com>
2016-09-23 18:27:42 +0300

    push Course

--- scripts/common.js
+++ scripts/common.js
@@ -10,4 +10,13 @@
         speed: 1000,
         galleryMargin: 40
     });
+    $('.me-teacher-slider').lightSlider({
+        item: 1,
+        vertical: true,
+        verticalHeight: 240,
+        controls: false,
+        pager: true,
+        slideMargin: 0,
+        speed: 800
+    });
 })();
