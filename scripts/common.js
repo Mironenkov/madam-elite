30838883f1479724862cfa1b6e78b0547a5573d0
Mironenkov Ruslan <mironenkovr@gmail.com>
2016-09-29 10:57:51 +0300

    some changes

--- scripts/common.js
+++ scripts/common.js
@@ -2,6 +2,7 @@
  * Created by GloryManU on 04.07.2016.
  */
 (function(){
+    //Sliders
     $('.me-reviews-slider').lightSlider({
         item: 1,
         controls: false,
@@ -19,4 +20,32 @@
         slideMargin: 0,
         speed: 800
     });
+    $('.me-why-slider').lightSlider({
+        item: 1,
+        controls: false,
+        pager: true,
+        slideMargin: 0,
+        speed: 1000,
+        galleryMargin: 60
+    });
+
+    ////Tabs
+    $('.me-form_classes-content--individual').hide();
+    $('.me-form_classe-tabs--group').addClass("me-form_classe-tabs--active");
+    $('.me-form_classe-tabs--group').click(function(){
+        $('.me-form_classe-tabs--individual').removeClass("me-form_classe-tabs--active");
+        $(this).addClass("me-form_classe-tabs--active");
+        $('.me-form_classes-content--individual').hide();
+        $('.me-form_classes-content--group').show("fast");
+        return false;
+    });
+    $('.me-form_classe-tabs--individual').click(function(){
+        $('.me-form_classe-tabs--group').removeClass("me-form_classe-tabs--active");
+        $(this).addClass("me-form_classe-tabs--active");
+        $('.me-form_classes-content--group').hide();
+        $('.me-form_classes-content--individual').show("fast");
+        return false;
+    });
+
+
 })();
